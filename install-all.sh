20dda7578293234f341d20fca778c624ab961cc1
Gustavo Abreu <16217546+gus-abreu@users.noreply.github.com>
2025-11-18 18:41:10 -0300

    added calibre and monitor fix

--- install-all.sh
+++ install-all.sh
@@ -4,12 +4,13 @@
 ./install-zsh.sh
 ./install-asdf.sh
 ./install-nodejs.sh
-./install-ruby.sh
-./install-postgresql.sh
+#./install-ruby.sh
+#./install-postgresql.sh
 ./install-ghostty.sh
 ./install-tmux.sh
 ./install-stow.sh
 ./install-dotfiles.sh
 ./install-hyprland-overrides.sh
+./install-calibre.sh
 
 ./set-shell.sh
